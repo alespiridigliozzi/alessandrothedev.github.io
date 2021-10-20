502200a4792f153b4c7d4e4fd04bda1b7eddd74c
alessandrothedev <spiridigliozzi.alessandro@gmail.com>
2021-10-20 13:47:18 +0100

    added footer section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AboutSection from './components/AboutSection/AboutSection';
 import ContactSection from './components/ContactSection/ContactSection';
 import FeaturedProjects from './components/FeaturedProjects/FeaturedProjects';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 
@@ -13,6 +14,7 @@ function App() {
         <AboutSection />
         <FeaturedProjects />
         <ContactSection />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.scss"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__logo"><a href="">Alessandro</a></div>
+            <div className="footer__links">
+                <a href="">Email</a> / <a href="">LinkedIn</a> / <a href="">Github</a>
+                </div>
+        </footer>
+    )
+}
+
+export default Footer
